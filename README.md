8dac79478026326c0332a6e7c571d15f855d0397
iam-hasibul <it.hhs19@gmail.com>
2021-03-18 03:47:34 +0600

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# youtube-audio-video-downloader
-A python script to download YouTube videos or audios.
+# Youtube Audio Video Downloader
+
+## Description:
+This is a python script to download YouTube videos or audios.
+
+## How to use:
+1. Install Python 3.8.X from [here](https://www.python.org/downloads/).
+2. Install youtube_dl `pip install youtube_dl`.
+3. Install tkinter `pip install tkinter`.
+4. Clone this repository: `git clone https://github.com/iam-hasibul/youtube-audio-video-downloader` or click `Download ZIP` in the right panel and extract it.
+5. Run `python Youtube-Audio-Video-Downloader.py` from the repository's directory.
+6. Choose between Audio Downloader and Video Downloader.
+7. Provide a link and click on 'Download' button. Downloaded file will be saved in your directory.
+
+## Screenshots:
+<img src="screenshots/screenshot-01.png" alt="Screenshot-01">
+<img src="screenshots/screenshot-02.png" alt="Screenshot-02">
+<img src="screenshots/screenshot-03.png" alt="Screenshot-03">
+<img src="screenshots/screenshot-04.png" alt="Screenshot-04">
---
+++ Youtube-Audio-Video-Downloader.py
@@ -0,0 +1,70 @@
+from tkinter import *
+from tkinter import messagebox
+from youtube_dl import YoutubeDL
+
+root = Tk()  # creating tkinter window
+root.iconphoto(False, PhotoImage(file='icon.png'))  # setting window icon
+root.title("Youtube Audio/Video Downloader")  # setting window title
+
+
+def audio():
+    root.geometry('500x250')  # setting window size
+    f1 = Frame().place(x=0, y=0, width=500, height=250)
+
+    # function to download audio
+    def downloader():
+        try:
+            url = str(link.get())
+            YoutubeDL({'format': 'bestaudio'}).extract_info(url)
+            Label(f1, text='Download Successful', font='roboto').place(relx=0.5, rely=0.8, anchor=CENTER)
+        except:
+            messagebox.showinfo(title='Warning', message='Enter Something!')
+
+    Label(f1, text='Youtube Audio Downloader', font=('roboto', 20)).place(relx=0.5, rely=0.1, anchor=CENTER)
+
+    link = StringVar()
+    Label(f1, text='Paste Link Here', font=('roboto', 15)).place(relx=0.5, rely=0.3, anchor=CENTER)
+    Entry(f1, width=70, textvariable=link).place(x=32, y=90)
+    Button(f1, text='Download', font=('roboto', 15), bg='red', fg='white', command=downloader).place(relx=0.4, rely=0.6,
+                                                                                                     anchor=CENTER)
+    Button(f1, text='Back', font=('roboto', 15), bg='black', fg='white', command=main_menu).place(relx=0.6, rely=0.6,
+                                                                                                  anchor=CENTER)
+
+
+def video():
+    root.geometry('500x250')  # setting window size
+    f1 = Frame().place(x=0, y=0, width=500, height=250)
+
+    # function to download video
+    def downloader():
+        try:
+            url = str(link.get())
+            YoutubeDL().download([url])
+            Label(f1, text='Download Successful', font='roboto').place(relx=0.5, rely=0.8, anchor=CENTER)
+        except:
+            messagebox.showinfo(title='Warning', message='Enter Something!')
+
+    Label(f1, text='Youtube Video Downloader', font=('roboto', 20)).place(relx=0.5, rely=0.1, anchor=CENTER)
+
+    link = StringVar()
+    Label(f1, text='Paste Link Here', font=('roboto', 15)).place(relx=0.5, rely=0.3, anchor=CENTER)
+    Entry(f1, width=70, textvariable=link).place(x=32, y=90)
+    Button(f1, text='Download', font=('roboto', 15), bg='red', fg='white', command=downloader).place(relx=0.4, rely=0.6,
+                                                                                                     anchor=CENTER)
+    Button(f1, text='Back', font=('roboto', 15), bg='black', fg='white', command=main_menu).place(relx=0.6, rely=0.6,
+                                                                                                  anchor=CENTER)
+
+
+def main_menu():
+    root.geometry('500x150')  # setting window size
+    f1 = Frame().place(x=0, y=0, width=500, height=250)
+    Label(f1, text='Youtube Audio/Video Downloader', font=('roboto', 20)).place(relx=0.5, rely=0.2, anchor=CENTER)
+
+    video_button = Button(f1, text='Audio Downloader', font=('roboto', 15), bg='red', fg='white', command=audio)
+    video_button.place(relx=0.3, rely=0.6, anchor=CENTER)
+    audio_button = Button(f1, text='Video Downloader', font=('roboto', 15), bg='red', fg='white', command=video)
+    audio_button.place(relx=0.7, rely=0.6, anchor=CENTER)
+
+
+main_menu()
+root.mainloop()
